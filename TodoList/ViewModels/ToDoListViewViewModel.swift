9fb0abbb23b46f9a5d020ebe9111553fa1a9dffb
Ozymandias089 <ozymandias089@gmail.com>
2023-12-03 12:55:04 +0900

    Secondary commit

--- TodoList/ViewModels/ToDoListViewViewModel.swift
+++ TodoList/ViewModels/ToDoListViewViewModel.swift
@@ -5,10 +5,28 @@
 //  Created by younghoonC on 2023/11/30.
 //
 
+import FirebaseFirestore
 import Foundation
 
+/// ViewModel for list of items view
+/// Primary tab
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
     
-    init() {}
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    func delete(id: String) {
+        let db = Firestore.firestore()
+        
+        /// Delete to do list item
+        /// - Parameter id: Item to delete
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
+    }
 }
--- TodoList/Views/ToDoListView.swift
+++ TodoList/Views/ToDoListView.swift
@@ -5,21 +5,32 @@
 //  Created by younghoonC on 2023/11/30.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
-    
-    private let  userId: String
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
-        self.userId = userId
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) {item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .tint(.red)
+
+                        }
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -38,6 +49,6 @@ struct ToDoListView: View {
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView(userId: "")
+        ToDoListView(userId: "sYBWKrVHxeY2mGRnJbtXO3Q8zZP2")
     }
 }
